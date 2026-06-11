a77b45b1d264d32d25ce190eef9674a6f54be91b
Sabrina Paschini <sabrina.paschini@hotmail.com>
2026-06-10 22:38:51 -0300

    hotfix/ mais tabelas

---
+++ Core/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Phone { get; set; }
+}
---
+++ Core/Entities/VehicleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities;
+
+public class VehicleStatus
+{
+    public int Id { get; set; }
+    public required string StatusName { get; set; } // disponivel, reservado, em manutencão e vendido 
+    public int VehicleId { get; set; }
+}
+
