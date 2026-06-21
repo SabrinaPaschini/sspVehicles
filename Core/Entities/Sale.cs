af0bfba7c515f64bebc857c9993616f814b50320
Sabrina Paschini <sabrina.paschini@hotmail.com>
2026-06-20 22:53:37 -0300

    hotfix/ add fk e modelando conforme fluxo

--- Core/Entities/Sale.cs
+++ Core/Entities/Sale.cs
@@ -6,7 +6,6 @@ public class Sale
     public int ProviderId { get; set; }
     public int CustomerId { get; set; }
     public decimal Total { get; set; }
-    public string ProductSale { get; set; }
     public Provider Provider { get; set; }
     public Customer Customer { get; set; }
     
--- Core/Entities/Vehicle.cs
+++ Core/Entities/Vehicle.cs
@@ -1,10 +1,11 @@
 ﻿namespace Core.Entities;
-
 public class Vehicle
 {
     public int VehicleId { get; set; }
-    public required string Model { get; set; }
-    public required int Year { get; set; }
-    public required string Automaker { get; set; }
-    public required decimal Price { get; set; }
+    public int Year { get; set; }
+    public string Automaker { get; set; }
+    public decimal Price { get; set; }
+
+    public int VehicleStatusId { get; set; }
+    public VehicleStatus VehicleStatus { get; set; }
 }
